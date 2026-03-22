76f309b1c8a51a7cb58c97363fade80c03bfc974
ivxn97 <104261052+ivxn97@users.noreply.github.com>
2026-03-23 06:01:37 +0800

    Implemented downloading of poster, other changes
    - Implemented downloading of poster using html2canvas and jsPDF. Poster is downloaded from a off-screen PosterView for highest quality. Refactored PosterView to use forwardRef for html2canvas ref.
    - Renamed "Album Cover Poster Generator" text titles to "Album Poster Generator" for better simplification.
    - Fixed mobile view being able to scroll, by adding overflow: hidden to a few css classes.
    - Renamed a few css classes to accommodate multiple design styles in future.
    - Migrated most inline styles to css classes.

--- src/components/posterView.tsx
+++ src/components/posterView.tsx
@@ -1,138 +1,142 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, forwardRef } from "react";
 import type SpotifyData from "../interfaces";
 import type SpotifyTrack from "../interfaces";
 
-const PosterView: React.FC<{
+type PosterViewProps = {
   album: SpotifyData;
   tracks: SpotifyTrack[];
   scale: number;
   albumColors: string[];
   selectedDesignIndex: number;
-}> = ({ album, tracks, scale, albumColors, selectedDesignIndex }) => {
-  const titleSizeRef = useRef<HTMLDivElement>(null);
-  const [titleFontSize, setTitleFontSize] = useState("36px");
+};
 
-  useEffect(() => {
-    if (titleSizeRef.current) {
-      let size = 36;
-      const el = titleSizeRef.current;
+const PosterView = forwardRef<HTMLDivElement, PosterViewProps>(
+  ({ album, tracks, scale, albumColors, selectedDesignIndex }, ref) => {
+    const titleSizeRef = useRef<HTMLDivElement>(null);
+    const [titleFontSize, setTitleFontSize] = useState("36px");
 
-      while (el.scrollWidth > el.clientWidth && size > 12) {
-        size -= 1;
-        el.style.fontSize = `${size}px`;
+    useEffect(() => {
+      if (titleSizeRef.current) {
+        let size = 36;
+        const el = titleSizeRef.current;
+
+        while (el.scrollWidth > el.clientWidth && size > 12) {
+          size -= 1;
+          el.style.fontSize = `${size}px`;
+        }
+
+        setTitleFontSize(`${size}px`);
       }
+    }, [album]);
 
-      setTitleFontSize(`${size}px`);
+    function groupBySeven(num: number) {
+      if (num <= 0) return 0;
+      return Math.ceil(num / 7);
     }
-  }, [album]);
 
-  function groupBySeven(num: number) {
-    if (num <= 0) return 0;
-    return Math.ceil(num / 7);
-  }
+    const numOfRows = groupBySeven(tracks.length);
+    let trackFontSize = "16px";
+    let columnGap = "20px";
 
-  const numOfRows = groupBySeven(tracks.length);
-  let trackFontSize = "16px";
-  let columnGap = "20px";
+    if (numOfRows === 3) {
+      trackFontSize = "14px";
+    } else if (numOfRows === 4) {
+      trackFontSize = "12px";
+    } else if (numOfRows >= 5) {
+      trackFontSize = "10px";
+      columnGap = "15px";
+    }
 
-  if (numOfRows === 3) {
-    trackFontSize = "14px";
-  } else if (numOfRows === 4) {
-    trackFontSize = "12px";
-  } else if (numOfRows >= 5) {
-    trackFontSize = "10px";
-    columnGap = "15px";
-  }
-
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexWrap: "wrap",
-        alignContent: "center",
-        justifyItems: "center",
-        justifyContent: "center",
-        zIndex: 10,
-      }}
-    >
+    return (
       <div
         style={{
-          width: 681 * scale,
-          height: 962 * scale,
-          position: "relative",
-          boxShadow: "rgba(0, 0, 0, 0.3) 0px 10px 30px",
+          display: "flex",
+          flexWrap: "wrap",
+          alignContent: "center",
+          justifyItems: "center",
+          justifyContent: "center",
         }}
       >
         <div
-          className="poster-view-container"
+          ref={ref}
           style={{
-            width: "679px",
-            height: "960px",
-            transform: `scale(${scale})`,
-            transformOrigin: "top left",
+            width: 681 * scale,
+            height: 962 * scale,
+            position: "relative",
+            boxShadow: "rgba(0, 0, 0, 0.3) 0px 10px 30px",
           }}
         >
-          <div className="poster-image-container">
-            <img src={album.image} alt={album.name} />
-          </div>
-          <div className="poster-bottom-design-container">
-            <div className="poster-title-container">
-              <div
-                className="flex-col"
-                style={{ width: "70%", justifyContent: "center", whiteSpace: "nowrap" }}
-              >
-                <span
-                  ref={titleSizeRef}
-                  className="album-title"
-                  style={{ fontSize: titleFontSize }}
-                >
-                  {album.name}
-                </span>
-                <span className="album-artist">{album.artist}</span>
-              </div>
-              <div
-                className="flex-col"
-                style={{ width: "30%", justifyContent: "center", paddingLeft: "1%" }}
-              >
+          <div
+            className="poster-view-container"
+            style={{
+              width: "679px",
+              height: "960px",
+              transform: `scale(${scale})`,
+              transformOrigin: "top left",
+            }}
+          >
+            <div className="poster-image-container">
+              <img src={album.image} alt={album.name} />
+            </div>
+            <div className="poster-bottom-container-style1">
+              <div className="poster-title-container-style1">
                 <div
-                  style={{
-                    height: "50%",
-                    display: "flex",
-                    flexDirection: "row",
-                    paddingRight: "1%",
-                  }}
+                  className="flex-col"
+                  style={{ width: "70%", justifyContent: "center", whiteSpace: "nowrap" }}
                 >
-                  {albumColors.map((color, index) => (
-                    <div
-                      key={index}
-                      style={{
-                        backgroundColor: color,
-                        height: "100%",
-                        width: `${100 / albumColors.length}%`,
-                        margin: "2px",
-                      }}
-                    />
-                  ))}
+                  <span
+                    ref={titleSizeRef}
+                    className="album-title"
+                    style={{ fontSize: titleFontSize }}
+                  >
+                    {album.name}
+                  </span>
+                  <span className="album-artist">{album.artist}</span>
+                </div>
+                <div
+                  className="flex-col"
+                  style={{ width: "30%", justifyContent: "center", paddingLeft: "1%" }}
+                >
+                  <div
+                    style={{
+                      height: "50%",
+                      display: "flex",
+                      flexDirection: "row",
+                      paddingRight: "1%",
+                    }}
+                  >
+                    {albumColors.map((color, index) => (
+                      <div
+                        key={index}
+                        style={{
+                          backgroundColor: color,
+                          height: "100%",
+                          width: `${100 / albumColors.length}%`,
+                          margin: "2px",
+                        }}
+                      />
+                    ))}
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="poster-tracklist-container">
-              <div
-                className="poster-tracklist"
-                style={{ columnCount: numOfRows, fontSize: trackFontSize, columnGap: columnGap }}
-              >
-                {tracks.map((track, index) => (
-                  <span key={index} className="poster-tracklist-item">
-                    {index + 1}. {track.name}
-                  </span>
-                ))}
+              <div className="poster-tracklist-container-style1">
+                <div
+                  className="poster-tracklist"
+                  style={{ columnCount: numOfRows, fontSize: trackFontSize, columnGap: columnGap }}
+                >
+                  {tracks.map((track, index) => (
+                    <span key={index} className="poster-tracklist-item">
+                      {index + 1}. {track.name}
+                    </span>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default PosterView;
