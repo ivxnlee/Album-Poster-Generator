70b54c24ff0ce7cefc04bb1d03523ad70a57a9d5
ivxn97 <104261052+ivxn97@users.noreply.github.com>
2026-03-26 23:49:36 +0800

    Converted into monorepo, added Sentry and Upstash
    - Added routes from backend into api folder. Refactored code to use vercel serverless functions instead of express.
    - Implemented Sentry into frontend and backend code.
    - Implemented Upstash Redis for token handling in spotifyAuth.js.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import * as Sentry from "@sentry/react";
 import "./index.css";
 import App from "./App.tsx";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+  initialScope: {
+    tags: { layer: "frontend" },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
