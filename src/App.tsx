b9fc3949809b397d6e14bfeb7c4cf189dea7e1d5
ivxn97 <104261052+ivxn97@users.noreply.github.com>
2026-03-27 03:45:25 +0800

    Fixed UI on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -224,7 +224,10 @@ function App() {
   };
 
   return (
-    <div className="outer-container" style={userFlow === 3 ? { width: scale * 829 } : {}}>
+    <div
+      className="outer-container"
+      style={userFlow === 3 ? { width: Math.min(window.innerWidth, scale * 829), padding: 0 } : {}}
+    >
       {isLoading && <LoadingOverlay />}
       {(userFlow === 1 || userFlow === 2) && (
         <>
