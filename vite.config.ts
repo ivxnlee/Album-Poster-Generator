b1d9c1973e5fdcb940d226eb09b45b1c3c93757b
ivxn97 <104261052+ivxn97@users.noreply.github.com>
2026-03-27 00:24:58 +0800

    Remove SSL requirements for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,10 +10,4 @@ export default defineConfig({
       },
     }),
   ],
-  server: {
-    https: {
-      key: fs.readFileSync("./SSL/localhost-key.pem"),
-      cert: fs.readFileSync("./SSL/localhost.pem"),
-    },
-  },
 });
